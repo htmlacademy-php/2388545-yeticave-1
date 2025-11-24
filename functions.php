529fd05f5a1972f36ac76a9652c4096b45f9ecde
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2025-11-24 11:16:02 +0300

    считает время окончания лота

--- functions.php
+++ functions.php
@@ -10,3 +10,52 @@ function format_price(int $price)
 {
     return number_format($price, 0, ',', ' ') . ' ₽';
 }
+
+/**
+ * Рассчитывает временной промежуток между двумя датами
+ *
+ * @param string $date Дата в фиде строки
+ * @return array Массив из двух строк, [0] - часы, [1] - минуты
+ */
+function get_dt_range(string $date)
+{
+    date_default_timezone_set('Europe/Moscow');
+
+    $cur_date = date_create("now");
+    $end_date = date_create($date);
+    $diff = date_diff($cur_date, $end_date);
+
+    $total_days = date_interval_format($diff, "%a");
+    $hours = date_interval_format($diff, "%h");
+    $minutes = date_interval_format($diff, "%i");
+
+    $total_hours = ($total_days * 24) + $hours;
+
+    $formatted_hours = str_pad($total_hours, 2, "0", STR_PAD_LEFT);
+    $formatted_minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+
+    return [$formatted_hours, $formatted_minutes];
+}
+
+/**
+ * Рассчитывает временной промежуток между двумя датами
+ *
+ * @param string $date Дата в фиде строки
+ * @return array Массив из двух строк, [0] - часы, [1] - минуты
+ */
+function get_date_range(string $date)
+{
+    date_default_timezone_set('Europe/Moscow');
+
+    $cur_date = strtotime("now");
+    $end_date = strtotime($date);
+    $diff = $end_date - $cur_date;
+
+    $hours = floor($diff / 3600);
+    $minutes = floor(($diff - $hours * 3600) / 60);
+
+    $formatted_hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    $formatted_minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+
+    return [$formatted_hours, $formatted_minutes];
+}
--- templates/main.php
+++ templates/main.php
@@ -15,24 +15,32 @@
     </div>
     <ul class="lots__list">
         <?php foreach ($lots as $lot): ?>
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="<?= $lot['img'] ?? 'img/placeholder.jpg' ?>" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category"><?= htmlspecialchars($lot['category'] ?? 'Прочее') ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($lot['title'] ?? 'Без названия') ?></a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= format_price($lot['price'] ?? 0) ?></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+            <?php $time_left = get_date_range($lot['date']) ?>
+            <?php $timer_class = (int)$time_left[0] < 1 ? "timer--finishing" : ""; ?>
+            <?php if ((int)$time_left[0] >= 0): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?= $lot['img'] ?? 'img/placeholder.jpg' ?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?= htmlspecialchars($lot['category'] ?? 'Прочее') ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($lot['title'] ?? 'Без названия') ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?= format_price($lot['price'] ?? 0) ?></span>
+                            </div>
+                            <div class="lot__timer timer <?= htmlspecialchars($timer_class) ?>">
+                                <?php if ((int)$time_left[0] < 0): ?>
+                                    00 : 00
+                                <?php else: ?>
+                                    <?= htmlspecialchars($time_left[0]) . " : " . htmlspecialchars($time_left[1]) ?>
+                                <?php endif; ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php endif; ?>
         <?php endforeach; ?>
     </ul>
 </section>
