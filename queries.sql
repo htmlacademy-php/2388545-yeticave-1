e7ea5669d4f2c9cc3a2c7ea55db965a4628411c2
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2025-12-08 10:40:51 +0300

    отрисовывает данные из БД

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@ INSERT INTO users (email, login, password, contact) VALUES
   ('angelina.ivanova@mail.ru', 'Angelina777', 123456, 89614565656),
   ('k.lastochkin@yandex.ru', 'Kostya95', 123456, 89053130031);
 
-INSERT INTO lots (name, description, image, start_price, end_date, step, user_id, category_id) VALUES
+INSERT INTO lots (name, description, img, start_price, end_date, step, user_id, category_id) VALUES
   ('2014 Rossignol District Snowboard', 'Сноуборд модель 2014 года', 'img/lot-1.jpg', 10999, '2026-12-01', 100, 1, 1),
   ('DC Ply Mens 2016/2017 Snowboard', 'Сноуборд в идеальном состоянии', 'img/lot-2.jpg', 159999, '2026-01-01', 200, 3, 1),
   ('Крепления Union Contact Pro 2015 года размер L/XL', 'Лучшие крепления в своей ценовой категории', 'img/lot-3.jpg', 8000, '2025-12-30 15:57', 1000, 2, 2),
@@ -33,7 +33,7 @@ SELECT * FROM categories;
 -- получить самые новые, открытые лоты.
 -- Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории;
 
-SELECT l.name, l.start_price, l.image, l.start_price, c.name as category_name FROM lots l JOIN categories c ON l.category_id = c.id WHERE l.end_date > NOW();
+SELECT l.name, l.start_price, l.img, l.start_price, c.name as category_name FROM lots l JOIN categories c ON l.category_id = c.id WHERE l.end_date > NOW();
 
 -- показать лот по его ID. Получите также название категории, к которой принадлежит лот;
 
--- templates/main.php
+++ templates/main.php
@@ -3,8 +3,8 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <?php foreach ($categories as $category): ?>
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($category) ?></a>
+            <li class="promo__item promo__item--<?= htmlspecialchars($category['slug']) ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($category['name']) ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
