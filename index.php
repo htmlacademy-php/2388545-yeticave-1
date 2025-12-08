7aedd00fca926504f84ea3664fd7ca99c34de6d8
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2025-12-08 23:57:16 +0300

    переименовывает переменные

--- index.php
+++ index.php
@@ -16,38 +16,38 @@ if (!$con) {
     // $lot_step = 500;
     // $user_id = 1;
     // $category_id = 1;
-    // $sqlLotsInsert = 'INSERT INTO lots (name, description, img, start_price, end_date, step, user_id, category_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-    // $stmtLots = mysqli_prepare($con, $sqlLotsInsert);
-    // mysqli_stmt_bind_param($stmtLots, 'sssisiii', $lot_name, $lot_description, $lot_img, $lot_start_price, $lot_end_date, $lot_step, $user_id, $category_id);
-    // mysqli_stmt_execute($stmtLots);
+    // $sql_lots_insert = 'INSERT INTO lots (name, description, img, start_price, end_date, step, user_id, category_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    // $stmt_lots = mysqli_prepare($con, $sql_lots_insert);
+    // mysqli_stmt_bind_param($stmt_lots, 'sssisiii', $lot_name, $lot_description, $lot_img, $lot_start_price, $lot_end_date, $lot_step, $user_id, $category_id);
+    // mysqli_stmt_execute($stmt_lots);
 
     // $category_name = 'Маски';
     // $category_slug = 'masks';
-    // $sqlCategoryInsert = 'INSERT INTO categories (name, slug) VALUES (?, ?)';
-    // $stmtCategory = mysqli_prepare($con, $sqlCategoryInsert);
-    // mysqli_stmt_bind_param($stmtCategory, 'ss', $category_name, $category_slug);
-    // mysqli_stmt_execute($stmtCategory);
+    // $sql_category_insert = 'INSERT INTO categories (name, slug) VALUES (?, ?)';
+    // $stmt_category = mysqli_prepare($con, $sql_category_insert);
+    // mysqli_stmt_bind_param($stmt_category, 'ss', $category_name, $category_slug);
+    // mysqli_stmt_execute($stmt_category);
 
-    $sqlLots = "SELECT l.name as title, l.start_price as price, l.img, l.end_date as date, c.name as category, l.start_date "
+    $sql_lots = "SELECT l.name as title, l.start_price as price, l.img, l.end_date as date, c.name as category, l.start_date "
         . "FROM lots l "
         . "JOIN categories c ON l.category_id = c.id "
         . "WHERE l.end_date > NOW() "
         . "ORDER BY l.start_date DESC "
         . "LIMIT 6";
 
-    $resultLots = mysqli_query($con, $sqlLots);
+    $result_lots = mysqli_query($con, $sql_lots);
 
-    if ($resultLots) {
-        $lots = mysqli_fetch_all($resultLots, MYSQLI_ASSOC);
+    if ($result_lots) {
+        $lots = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
     } else {
         print("Ошибка MySQL: " . mysqli_error($con));
     }
 
-    $sqlCategories = "SELECT name, slug FROM categories";
-    $resultCategories = mysqli_query($con, $sqlCategories);
+    $sql_categories = "SELECT name, slug FROM categories";
+    $result_categories = mysqli_query($con, $sql_categories);
 
-    if ($resultCategories) {
-        $categories = mysqli_fetch_all($resultCategories, MYSQLI_ASSOC);
+    if ($result_categories) {
+        $categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);
     } else {
         print("Ошибка MySQL: " . mysqli_error($con));
     }
