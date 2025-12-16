f30bc6ef8d0ba07de9f5c3b1a6c87e3ffbd62c74
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2025-12-16 14:23:54 +0300

    правит ошибку

--- repository/sql-lot.php
+++ repository/sql-lot.php
@@ -6,10 +6,14 @@ function get_lot(mysqli $con, int $lot_id)
         SELECT l.id, l.name as title, l.start_price as price, l.img, l.description, l.end_date as date, c.name as category
         FROM lots l
         JOIN categories c ON l.category_id = c.id
-        WHERE l.id = $lot_id
+        WHERE l.id = ?
     SQL;
 
-    $result_lot = mysqli_query($con, $sql_lot);
+    $stmt = db_get_prepare_stmt($con, $sql_lot, [$lot_id]);
+
+    mysqli_stmt_execute($stmt);
+
+    $result_lot = mysqli_stmt_get_result($stmt);
 
     if (!$result_lot) {
         echo "Произошла ошибка MySQL";
